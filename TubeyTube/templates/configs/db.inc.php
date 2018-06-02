46a6b6688b3d5db3f31d538e372c8e9249fef2b3
Mehmet Ozdemir <whosforeign@gmail.com>
2018-06-02 02:14:40 -0400

    Update db.inc.php

--- TubeyTube/templates/configs/db.inc.php
+++ TubeyTube/templates/configs/db.inc.php
@@ -1,9 +1,9 @@
 <?php
-$serverName ="shareddb-g.hosting.stackcp.net";
-$userName = "tubeydb-323573e4";
-$passWord = "46b9g8lyaz";
-$dbName = "tubeydb-323573e4";
-$conn = mysqli_connect($serverName,$userName,$passWord,$dbName);
+$serverName ="";
+$userName = "";
+$passWord = "";
+$dbName = "";
+$conn = ;
 
 
 ?>
