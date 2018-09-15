1e01c7cf3280802b85f4c35543b06d5bebf5c2ed
Mehmet Ozdemir <whosforeign@gmail.com>
2018-09-15 15:44:39 -0400

    No duplicates
    Got rid of duplicate first video showing up

--- TubeyTube/templates/deleteVideo.php
+++ TubeyTube/templates/deleteVideo.php
@@ -2,4 +2,6 @@
 require 'configs/db.inc.php';
 $min_id = "DELETE FROM videoId ORDER BY id LIMIT 1;";
 $result = mysqli_query($conn, $min_id);
+$num_of_cookies = $_COOKIE['rows'];
+setcookie('rows', $num_of_cookies-1, time() + (86400 * 30), "/");
 ?>
--- TubeyTube/templates/updateVideo.php
+++ TubeyTube/templates/updateVideo.php
@@ -2,8 +2,24 @@
 require 'configs/db.inc.php';
 $sql = "SELECT ytID,title FROM videoId ORDER BY id;";
 $result = mysqli_query($conn, $sql);
-while($rows = mysqli_fetch_assoc($result)){
-    echo "<li class=white-text id=".$rows['ytID'].">• ".$rows['title']."</li>";
+$result_length = mysqli_num_rows($result);
+if($result_length!=1){
+    $pass_first = 0;
+    while($rows = mysqli_fetch_assoc($result)){
+        if($pass_first!=0){
+            echo "<li class=white-text id=".$rows['ytID'].">• ".$rows['title']."</li>";
+        }
+        $pass_first = $pass_first + 1;
+    }
+}
+else{
+    $pass_first = 0;
+    while($rows = mysqli_fetch_assoc($result)){
+        if($pass_first!=0){
+            echo "<li class=white-text id=".$rows['ytID'].">• ".$rows['title']."</li>";
+        }
+        $pass_first = $pass_first + 1;
+    }
 }
 
 ?>
